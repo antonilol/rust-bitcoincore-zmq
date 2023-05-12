9a26962182fac0f130bce7e6a182cc5a19ab0dbf
Antoni Spaanderman <56turtle56@gmail.com>
2023-05-12 23:59:07 +0200

    format lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,15 @@ mod message;
 mod sequence_message;
 mod subscribe;
 
-pub use crate::error::Error;
-pub use crate::message::Message;
-pub use crate::sequence_message::SequenceMessage;
-#[allow(deprecated)]
-pub use crate::subscribe::{
-    sub_zmq, subscribe_multi, subscribe_multi_blocking, subscribe_single,
-    subscribe_single_blocking, Action,
+pub use crate::{
+    error::Error,
+    message::Message,
+    sequence_message::SequenceMessage,
+    subscribe::{
+        subscribe_multi, subscribe_multi_blocking, subscribe_single, subscribe_single_blocking,
+        Action,
+    },
 };
+
+#[allow(deprecated)]
+pub use crate::subscribe::sub_zmq;
